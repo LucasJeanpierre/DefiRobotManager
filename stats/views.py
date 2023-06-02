42d5ac29a5693cac17d68fb61040f697ad8056f8
LucasJeanpierre <61747163+LucasJeanpierre@users.noreply.github.com>
2023-06-02 19:26:58 +0200

    Remove unnecessary import

--- stats/views.py
+++ stats/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.db.models import Max
-from django.contrib.auth.decorators import login_required
 import random
 
 from .models import Institution, Team, AesteticScores, Run
